bfb967b539b5396f1334d36a1d87793ce16082f3
Neveen Abd El Azeem <neveenhassannew@gmail.com>
2022-07-05 22:07:19 +0200

    Adding Files

---
+++ SampleApi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sampleclasslibrary;
+
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        Calc mycalc = new Calc();
+
+        [HttpGet("/average")]
+        public int Average([FromQuery] int[] nums)
+        {
+           return mycalc.Average(nums);
+        }
+
+        [HttpGet("/smallest")]
+        public int Smallest([FromQuery] int[] nums)
+        {
+           return mycalc.Smallest(nums);
+        }
+
+        [HttpGet("/largest")]
+        public int Largest([FromQuery] int[] nums)
+        {
+           return mycalc.Largest(nums);
+
+        }
+    }
+}
